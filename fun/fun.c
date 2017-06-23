62a6c67a07e6d6453f1965e6f465014987fb5ffe
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-06-23 17:49:11 +0300

    More fun with fun

--- fun/fun.c
+++ fun/fun.c
@@ -5,16 +5,19 @@
 
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 
 
 //=================================================
+/// Print an error message and exit
 static void fatal(const char * msg)
 {
     fputs(msg, stderr);
     exit(1);
 }
 //=================================================
+/// Print a 2-part error message and exit
 static void fatal2(const char * msg1, const char * msg2)
 {
     fprintf(stderr, "%s %s\n", msg1, msg2);
@@ -29,14 +32,6 @@ int main(int argc, char* argv[])
     // File name + default
     char fileName[500] = "/home/kaa-dev/grechnyev/stream/media-samp/Suteki.mp4";
 
-    // Vars, init to NULL just in case
-    AVFormatContext * pFormatCtx = NULL;
-    AVCodecContext * pCodexCtxOrig = NULL;
-    AVCodecContext * pCodexCtx = NULL;
-
-
-    //---------
-
     if (argc > 1 && strlen(argv[1]) < 500)
     {
         // Set file name
@@ -47,6 +42,7 @@ int main(int argc, char* argv[])
     av_register_all();
 
     // Open the video file
+    AVFormatContext * pFormatCtx = NULL;
     if (avformat_open_input(&pFormatCtx, fileName, NULL, NULL) != 0)
         fatal2("Cannot open input file ", fileName);
 
@@ -72,6 +68,74 @@ int main(int argc, char* argv[])
         fatal("Video stream not found");
 
 
+    // Get codec parameters
+    AVCodecParameters * pCodecPar = pFormatCtx->streams[videoStream]->codecpar;
+
+    // Find the decoder
+    AVCodec *pCodec = avcodec_find_decoder(pCodecPar->codec_id);
+    if (pCodec == NULL)
+        fatal("Cannot find decoder");
+
+    // Create context out of codec parameters
+    AVCodecContext * pCodecCtx = avcodec_alloc_context3(pCodec);
+    if (avcodec_parameters_to_context(pCodecCtx, pCodecPar) < 0)
+        fatal("Cannot create codec context from parameters");
+
+    // Open codec
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+        fatal("Cannot open codec");
+
+    //------------------------------------------------------
+    // Now, the frame
+    // Video frame
+    AVFrame * pFrame = av_frame_alloc();
+    // RGB frame
+    AVFrame * pFrameRGB = av_frame_alloc();
+    if (pFrame==NULL || pFrameRGB==NULL)
+        fatal("Cannot allocate frame");
+
+
+    // Frame width and height
+    int frWidth = pCodecCtx->width;
+    int frHeight = pCodecCtx->height;
+
+    // Allocate buffer
+    // Replaced another deprecated function, the result is actually width*height*3, how simple !
+    int bufSize = av_image_get_buffer_size(AV_PIX_FMT_RGB24, frWidth, frHeight, 1);
+    printf("width = %d, height = %d, numBytes = %d \n", frWidth, frHeight, bufSize);
+    uint8_t * buffer = (uint8_t *)av_malloc(bufSize * sizeof(uint8_t));
+
+
+    // Assign appropriate parts of buffer to image planes in pFrameRGB
+    // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
+    // of AVPicture
+    avpicture_fill((AVPicture *)pFrameRGB, buffer, AV_PIX_FMT_RGB24, frWidth, frHeight);
+//    av_image_fill_arrays()
+
+    // Set the SWS context. SWS is the SoftWare Scale library, here apparently we don't change size
+    // But recode from the input pixel formal (YUV or whatever) to RGB
+    struct SwsContext *swsCtx = sws_getContext(frWidth, frHeight, pCodecCtx->pix_fmt, frWidth, frHeight, AV_PIX_FMT_RGB24,
+                            SWS_BILINEAR, NULL, NULL, NULL);
+
+    //------------------------------------------------------
+    // Finally, the Big Frame loop, we read a frame and process it
+
+    int i = 0; // Frame number
+    int frameFinshed; // bool: decoded successfully ?
+    AVPacket packet;
+
+    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+        if (packet.stream_index == videoStream) {
+            // Decode video frame
+            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinshed, &packet);
+            ++i; // Increase frame count
+
+
+        }
+    }
+
+
+    puts("Goblins are finishing !");
 
     return 0;
 }
